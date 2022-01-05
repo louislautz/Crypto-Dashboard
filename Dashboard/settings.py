bfa0e322d138ddfe9c8348691ed4d7daa75b75af
louislautz <louislautz@gmx.net>
2022-01-05 22:10:10 +0100

    Refactored app to match common Flask practices.

---
+++ Dashboard/settings.py
@@ -0,0 +1,5 @@
+SQLALCHEMY_DATABASE_URI = 'sqlite:///Data/SQLite/BuyTransactions.sqlite3'
+SQLALCHEMY_BINDS = {
+    'SellTransactions': 'sqlite:///Data/SQLite/SellTransactions.sqlite3'
+}
+SQLALCHEMY_TRACK_MODIFICATIONS = False
